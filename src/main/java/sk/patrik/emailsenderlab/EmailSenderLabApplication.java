0e5181921fc047bea00fd92fa094c741782889d9
Patrik Scerba <patrikscerba@gmail.com>
2026-06-14 08:52:11 +0200

    chore(project): inicializacia Email Sender Lab projektu

---
+++ src/main/java/sk/patrik/emailsenderlab/EmailSenderLabApplication.java
@@ -0,0 +1,13 @@
+package sk.patrik.emailsenderlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSenderLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/patrik/emailsenderlab/EmailSenderLabApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.patrik.emailsenderlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSenderLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
