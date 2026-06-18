9a521f166892c88982aac433f86b662c438ea3cf
Patrik Scerba <patrikscerba@gmail.com>
2026-06-18 16:01:40 +0200

    feat(email): pridanie logiky odosielania textoveho emailu

--- src/main/java/sk/patrik/emailsenderlab/service/EmailService.java
+++ src/main/java/sk/patrik/emailsenderlab/service/EmailService.java
@@ -1,4 +1,12 @@
 package sk.patrik.emailsenderlab.service;
 
+import sk.patrik.emailsenderlab.dto.EmailRequest;
+
+/**
+ *  Definuje základné operácie pre odosielanie emailov.
+ */
 public interface EmailService {
+
+    // Odošle jednoduchý textový email.
+    void sendTextEmail(EmailRequest emailRequest);
 }
--- src/main/java/sk/patrik/emailsenderlab/service/EmailServiceImpl.java
+++ src/main/java/sk/patrik/emailsenderlab/service/EmailServiceImpl.java
@@ -1,4 +1,44 @@
 package sk.patrik.emailsenderlab.service;
 
-public class EmailServiceImpl {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import sk.patrik.emailsenderlab.dto.EmailRequest;
+
+/**
+ * Servisná implementácia zodpovedná za odosielanie emailov.
+ * Táto trieda používa Spring komponent JavaMailSender, ktorý zabezpečuje
+ * samotné odoslanie emailovej správy cez nakonfigurovaný SMTP server.
+ */
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    // Emailová adresa odosielateľa načítaná z konfigurácie aplikácie.
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    // Konštruktor vloží JavaMailSender pripravený podľa SMTP konfigurácie.
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    // Metóda vytvorí jednoduchú textovú emailovú správu a odošle ju cez JavaMailSender.
+    @Override
+    public void sendTextEmail(EmailRequest emailRequest) {
+
+        // Vytvorenie emailovej správy.
+        SimpleMailMessage emailMessage = new SimpleMailMessage();
+
+        // Nastavenie základných údajov emailu.
+        emailMessage.setFrom(fromEmail);
+        emailMessage.setTo(emailRequest.getTo());
+        emailMessage.setSubject(emailRequest.getSubject());
+        emailMessage.setText(emailRequest.getMessage());
+
+        // Odoslanie emailu cez nakonfigurovaný SMTP server.
+        javaMailSender.send(emailMessage);
+    }
 }
