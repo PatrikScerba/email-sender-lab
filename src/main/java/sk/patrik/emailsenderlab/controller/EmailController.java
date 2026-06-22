8c501cd036b72a3688314adbace532b6b2ba52fd
Patrik Scerba <patrikscerba@gmail.com>
2026-06-22 14:35:55 +0200

    feat(email): pridanie endpointu pre odosielanie HTML emailu

--- src/main/java/sk/patrik/emailsenderlab/controller/EmailController.java
+++ src/main/java/sk/patrik/emailsenderlab/controller/EmailController.java
@@ -2,10 +2,7 @@ package sk.patrik.emailsenderlab.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sk.patrik.emailsenderlab.dto.EmailRequest;
 import sk.patrik.emailsenderlab.service.EmailService;
 
@@ -30,5 +27,12 @@ public class EmailController {
     public ResponseEntity<String> sendTextEmail(@Valid @RequestBody EmailRequest emailRequest) {
         emailService.sendTextEmail(emailRequest);
         return ResponseEntity.ok("Email bol úspešne odoslaný.");
+    }
+
+    // Endpoint na odoslanie HTML emailu vytvoreného cez Thymeleaf šablónu.
+    @PostMapping("/send-html")
+    public ResponseEntity<String> sendHtmlEmail(@Valid @RequestBody EmailRequest emailRequest) {
+        emailService.sendHtmlEmail(emailRequest);
+        return ResponseEntity.ok("HTML email bol úspešne odoslaný.");
     }
 }
