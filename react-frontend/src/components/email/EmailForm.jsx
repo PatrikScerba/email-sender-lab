39de20e50879ed2dceb5d7feef342d322bec155d
Patrik Scerba <patrikscerba@gmail.com>
2026-07-08 14:11:35 +0200

    Pridany formular na odoslanie textoveho emailu

---
+++ react-frontend/src/components/email/EmailForm.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { sendTextEmail } from "../../api/emailApi";
+
+export default function EmailForm({ emailType }) {
+  const emailTitle = {
+    text: "Odoslať textový email",
+  };
+
+  const [emailData, setEmailData] = useState({
+    to: "",
+    subject: "",
+    message: "",
+  });
+
+  const [info, setInfo] = useState("");
+  const [error, setError] = useState("");
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setEmailData({
+      ...emailData,
+      [name]: value,
+    });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setInfo("");
+    setError("");
+
+    try {
+      if (emailType === "text") {
+        const responseMessage = await sendTextEmail(emailData);
+        setInfo(responseMessage);
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
+  return (
+    <div>
+      <h2>{emailTitle[emailType]}</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          style={{ marginRight: "20px" }}
+          type="email"
+          name="to"
+          placeholder="Príjemca"
+          value={emailData.to}
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          name="subject"
+          placeholder="Predmet"
+          value={emailData.subject}
+          onChange={handleChange}
+          style={{ marginRight: "20px" }}
+        />
+
+        <textarea
+          name="message"
+          placeholder="Správa"
+          value={emailData.message}
+          onChange={handleChange}
+          style={{ marginRight: "20px" }}
+        />
+
+        <button style={{ marginRight: "15px" }} type="submit">
+          {emailTitle[emailType]}
+        </button>
+
+        {info && <p>{info}</p>}
+        {error && <p>{error}</p>}
+      </form>
+    </div>
+  );
+}
