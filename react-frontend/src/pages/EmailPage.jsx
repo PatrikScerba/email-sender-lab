75284d846042af32b24c142b58847f76f3aeb65a
Patrik Scerba <patrikscerba@gmail.com>
2026-07-09 13:21:56 +0200

    feat(react-email):Pripojeny emailovy formular na stránku

--- react-frontend/src/pages/EmailPage.jsx
+++ react-frontend/src/pages/EmailPage.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import EmailTypeSelector from "../components/email/EmailTypeSelector";
+import EmailForm from "../components/email/EmailForm";
 
 export default function EmailPage() {
   const [emailType, setEmailType] = useState("text");
 
   return (
-    <EmailTypeSelector emailType={emailType} setEmailType={setEmailType} />
+    <>
+      <EmailTypeSelector emailType={emailType} setEmailType={setEmailType} />
+      <EmailForm emailType={emailType} />
+    </>
   );
 }
-
