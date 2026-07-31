db6657447acd0ce2876904b7f152d1000591d01b
Patrik Scerba <patrikscerba@gmail.com>
2026-07-31 13:56:39 +0200

    feat(react-ui): napojenie hlavicky do emailovej stranky

--- react-frontend/src/pages/EmailPage.jsx
+++ react-frontend/src/pages/EmailPage.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import EmailTypeSelector from "../components/email/EmailTypeSelector";
 import EmailForm from "../components/email/EmailForm";
+import AppHeader from "../components/layout/AppHeader.jsx";
 import "./EmailPage.css";
 
 export default function EmailPage() {
   const [emailType, setEmailType] = useState("text");
 
   return (
-    <>
-      <EmailTypeSelector emailType={emailType} setEmailType={setEmailType} />
-      <EmailForm emailType={emailType} />
-    </>
+    <main className="email-page">
+      <section className="email-page__content">
+        <AppHeader />
+
+        <EmailTypeSelector emailType={emailType} setEmailType={setEmailType} />
+
+        <EmailForm emailType={emailType} />
+      </section>
+    </main>
   );
 }
