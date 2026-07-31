58eab11291ffbea3b8df24effcfab182c14d3dff
Patrik Scerba <patrikscerba@gmail.com>
2026-07-31 13:35:17 +0200

    feat(react-ui): pridanie stylov emailovej stranky

--- react-frontend/src/pages/EmailPage.jsx
+++ react-frontend/src/pages/EmailPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import EmailTypeSelector from "../components/email/EmailTypeSelector";
 import EmailForm from "../components/email/EmailForm";
+import "./EmailPage.css";
 
 export default function EmailPage() {
   const [emailType, setEmailType] = useState("text");
